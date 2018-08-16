a6beb1d10c0ae9cf0c16a4ec7774a839be93e796
German Grandas <gertiven@gmail.com>
2018-08-16 09:25:33 -0500

    maquetación login

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Row , Input , Button} from 'react-materialize';
 
 class Login extends Component{
     constructor(props) {
@@ -10,12 +10,31 @@ class Login extends Component{
             password : ''
         }
     }
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {        
+        this.setState({
+            [event.target.type] : event.target.value
+        });
+    }
+    
+    handleSubmit = event => {
+        console.log(this.state);
+        event.preventDefault();
+        
+    }
 
     render() {
       return (
-        <div>
-          Hello World!
-        </div>
+        <Row className='Login'>
+            <Input type="email" s={6} label="Email" value={this.state.email} onChange={this.handleChange}/>
+            <Input type="password" label="password" s={6} value={this.state.password} onChange={this.handleChange}/>
+            <Button waves='light' onClick={this.handleSubmit} disabled={!this.validateForm()}>Summit</Button>
+        </Row>
       )
     };
 }
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Login from './components/login';
 const App = ()=>{
     return (
-        <div>
-            Hello
-        </div>
+        <Login/>
     )
 }
 
