834a7348f67ae51460ccb6eb7bf26d081b44095f
German Grandas <gertiven@gmail.com>
2018-08-18 15:11:45 -0500

    Captcha Añadido

--- src/components/login.js
+++ src/components/login.js
@@ -1,38 +1,62 @@
 import React, {Component} from 'react';
 import {Row , Input , Button} from 'react-materialize';
+import Recaptcha from 'react-recaptcha';
+
+
+const sitekey='6LdwoGoUAAAAAOIjSUoj1TO5KKeDEt-TBKs2oHXz';
+let recaptchaInstance;
 
 class Login extends Component{
     constructor(props) {
         super(props);
 
+        
         this.state = {
             email : '',
-            password : ''
+            password : '',
+            captcha : false
         }
-    }
-    validateForm() {
-        return this.state.email.length > 0 && this.state.password.length > 0;
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.checked = this.checked.bind(this);
     }
 
-    handleChange = event => {        
-        this.setState({
-            [event.target.type] : event.target.value
-        });
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0 && this.state.captcha;
     }
     
-    handleSubmit = event => {
-        console.log(this.state);
-        event.preventDefault();
-        
+    resetRecaptcha = () => {
+        this.recaptchaInstance.reset();
+        this.setState({captcha:false}) 
+      };
+
+    handleChange = event => {
+        console.log(event.target.name);
+           
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+    }
+    checked = event=>{
+        this.setState({captcha:true});
     }
 
+    handleSubmit = event => {
+        alert('A name was submitted: ' + this.state.email +' '+ this.state.password);
+        this.resetRecaptcha();
+        event.preventDefault(); 
+    }
     render() {
       return (
-        <Row className='Login'>
-            <Input type="email" s={6} label="Email" value={this.state.email} onChange={this.handleChange}/>
-            <Input type="password" label="password" s={6} value={this.state.password} onChange={this.handleChange}/>
-            <Button waves='light' onClick={this.handleSubmit} disabled={!this.validateForm()}>Summit</Button>
-        </Row>
+        <form onSubmit={this.handleSubmit}>
+            <Row className='Login'>
+                <Input name='email' className='input' type="email" s={10}  label="Email" value={this.state.email} onChange={this.handleChange}/>
+                <Input name='password' className='input' type="password" label="password" s={10} value={this.state.password} onChange={this.handleChange}/>
+                <Recaptcha ref={e => recaptchaInstance = e} className='captcha' s={10} sitekey={sitekey} render="explicit" verifyCallback={this.checked}/>
+                <Button className='boton' s={11} waves='light'  disabled={!this.validateForm()}>Sign Up</Button>
+            </Row>
+            
+        </form>
       )
     };
 }
