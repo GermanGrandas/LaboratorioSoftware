c039eaff3f70d40f98f557ea08497a7caab92c73
MauroCG <mauro57.mauricio@gmail.com>
2019-02-13 19:37:59 -0500

    Header del index responsivo (Para diferente tamaños y para navegador chrome y firefox)
    Se hizó una maquetación con caja flexible a una columna y con espaciado proporcionado.
    Para que el espaciado proporcionado funcionase se debió encerrar tanto la imagen como el header en cajas individuales (dentro de una div cada uno).
    El diseño de la página no se cambió.

--- src/components/TopComponent/index.js
+++ src/components/TopComponent/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {Image} from 'semantic-ui-react'
 import './index.css';
 
 const TopComp = ()=>{
     return(
                 <div className={'containerHeader'}>
-                <img src={require('../images/logo_docent.png')} alt='Logo DocentHelper' />
-                <center>
-                <header
+                <div><img src={require('../images/logo_docent.png')} alt='Logo DocentHelper' /></div>
+                <div><header
                     as='h1'
-                >DocentHelper</header>
-                </center>
+                >DocentHelper</header></div>
                 </div> 
         
     )
